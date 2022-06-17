9be2fdc593c7c150302cff83d06a9905c11490fb
crhicko <crhicko@protonmail.com>
2022-06-17 09:59:46 -0400

    simple js connection and subscription

--- src/code/persist.js
+++ src/code/persist.js
@@ -1,13 +1,24 @@
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
-console.log("I ALIVE")
+import { connect, StringCodec } from "nats";
+
+const nc = await connect({ servers: "host.docker.internal:4222"})
+
+console.log(nc)
+
+const sc = StringCodec();
+
+const cb = (err, msg) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(sc.decode(msg.data))
+	}
+}
+
+const sub_sport = nc.subscribe("sports_event", {
+		callback: cb
+	})
+const sub_exec = nc.subscribe("execution", {
+		callback: cb
+	})
+
+
